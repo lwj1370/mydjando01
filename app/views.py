b64e2308b79cda3e2c96c6be074230c6480b9a89
WooJin Lee <lwj1370@naver.com>
2022-08-06 23:46:57 +0900

    post 상세 페이지 추가

--- app/views.py
+++ app/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from app.models import Post
 
 
 def index(request: HttpRequest) -> HttpResponse:
-
     qs = Post.objects.all()
     return render(request, "app/index.html", {"post_list": qs})
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "app/post_detail.html", {"post": post})
