cc915c47d7392f888d1e73c7914c0d81bff80260
WooJin Lee <lwj1370@naver.com>
2022-08-09 00:47:09 +0900

    Custom command 파일 추가 및 이메일 템플릿 문구 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = Env()
 env_path = BASE_DIR / ".env"
 if env_path.exists():
-    with env_path.open("rtm", encoding="utf8") as f:
+    with env_path.open("rt", encoding="utf8") as f:
         env.read_env(f, overwrite=True)
 
 # Quick-start development settings - unsuitable for production
