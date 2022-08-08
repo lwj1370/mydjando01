993d70fccab0eb7da4c1290b725d1c52222d247e
WooJin Lee <lwj1370@naver.com>
2022-08-08 23:30:23 +0900

    django-bootstrap5 적용 및 post_new 뷰 구현

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
 
+from app.forms import PostForm
 from app.models import Post
 
 
@@ -12,3 +14,6 @@ def index(request: HttpRequest) -> HttpResponse:
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     post = get_object_or_404(Post, pk=pk)
     return render(request, "app/post_detail.html", {"post": post})
+
+
+post_new = CreateView.as_view(model=Post, form_class=PostForm, success_url="/app/")
